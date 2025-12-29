7dcf7b321a490423a3973914fb35f5851f6727bf
Lars Hegmann <99564048+hegi64@users.noreply.github.com>
2025-12-29 05:32:17 +0100

    modify the linux ids (#4)

--- pkg/minimk3/ids_linux.go
+++ pkg/minimk3/ids_linux.go
@@ -3,7 +3,7 @@ package minimk3
 import "regexp"
 
 var (
-	rxDAW     = regexp.MustCompile(`Launchpad Mini MK3:Launchpad Mini MK3 MIDI 2`)
-	rxMidiIn  = regexp.MustCompile(`Launchpad Mini MK3:Launchpad Mini MK3 MIDI 1`)
-	rxMidiOut = regexp.MustCompile(`Launchpad Mini MK3:Launchpad Mini MK3 MIDI 1`)
+	rxDAW     = regexp.MustCompile(`Launchpad Mini MK3:Launchpad Mini MK3 (MIDI 2|LPMiniMK3 DA)`)
+	rxMidiIn  = regexp.MustCompile(`Launchpad Mini MK3:Launchpad Mini MK3 (MIDI 1|LPMiniMK3 MI)`)
+	rxMidiOut = regexp.MustCompile(`Launchpad Mini MK3:Launchpad Mini MK3 (MIDI 1|LPMiniMK3 MI)`)
 )
